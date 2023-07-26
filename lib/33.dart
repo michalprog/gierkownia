ab4e80fef4765f5265768eb1f29600860937f7d3
michalprog <michalch001@gmail.com>
2023-07-26 15:36:27 +0200

    dodanie33

---
+++ lib/33.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+class Gra33 extends StatefulWidget {
+  const Gra33({Key? key}) : super(key: key);
+
+  @override
+  State<Gra33> createState() => _Gra33State();
+}
+
+class _Gra33State extends State<Gra33> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
