e379d0ebcb7e886d2742ffff0686fd012f00b251
michalprog <michalch001@gmail.com>
2023-07-26 14:35:25 +0200

    poczatek

---
+++ android/app/src/main/kotlin/com/example/gierkownia/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.gierkownia
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Glowna());
+}
+
+class Glowna extends StatefulWidget {
+  const Glowna({Key? key}) : super(key: key);
+
+  @override
+  State<Glowna> createState() => _GlownaState();
+}
+
+class _GlownaState extends State<Glowna> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
