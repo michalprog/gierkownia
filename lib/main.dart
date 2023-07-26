5d03c90627aa3529d076ba94a45423d0f13f259c
michalprog <michalch001@gmail.com>
2023-07-26 19:08:07 +0200

    zmiana na stateless

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:gierkownia/33.dart';
+import 'package:gierkownia/Warcaby_Screen.dart';
+import 'package:gierkownia/tictactoe.dart';
+import 'package:gierkownia/Szachy_screen.dart';
 
 void main() {
   runApp(const Glowna());
 }
 
-class Glowna extends StatefulWidget {
+class Glowna extends StatelessWidget {
   const Glowna({Key? key}) : super(key: key);
-
-  @override
-  State<Glowna> createState() => _GlownaState();
-}
-
-class _GlownaState extends State<Glowna> {
-  void Change_Page(int type) {}
 
   @override
   Widget build(BuildContext context) {
@@ -25,90 +22,95 @@ class _GlownaState extends State<Glowna> {
           ),
           body: Center(
               child: Column(
-            children: [
-              const SizedBox(
-                height: 40,
-              ),
-              Container(
-                width: 300,
-                padding: EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                children: [
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        "Gra 33",
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "Gra 33",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-              Container(
-                width: 300,
-                padding: EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(builder: (context) => Gra33()),
+                        );
+                      },
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        "kólko i krzyżyk",
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "kólko i krzyżyk",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-              Container(
-                width: 300,
-                padding: EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        "warcaby",
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "warcaby",
-                    style: TextStyle(fontSize: 18),
-                  ),
-                ),
-              ),
-              Container(
-                width: 300,
-                padding: EdgeInsets.all(20),
-                child: ElevatedButton(
-                  onPressed: () {},
-                  style: ElevatedButton.styleFrom(
-                    padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
+                  Container(
+                    width: 300,
+                    padding: EdgeInsets.all(20),
+                    child: ElevatedButton(
+                      onPressed: () {},
+                      style: ElevatedButton.styleFrom(
+                        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 15),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        "Szachy",
+                        style: TextStyle(fontSize: 18),
+                      ),
                     ),
                   ),
-                  child: Text(
-                    "Szachy",
-                    style: TextStyle(fontSize: 18),
+                  const SizedBox(
+                    height: 80,
                   ),
-                ),
-              ),
-              const SizedBox(
-                height: 80,
-              ),
-              Center(
-                  child: Text("twórca: Michał Chojnacki",
-                      style: TextStyle(
-                        fontSize: 20,
-                        color:Colors.deepPurple
+                  Center(
+                      child: Text("twórca: Michał Chojnacki",
+                          style: TextStyle(
+                              fontSize: 20,
+                              color:Colors.deepPurple
 
-                      ))),
-            ],
-          ))),
+                          ))),
+                ],
+              ))),
     );
   }
 }
