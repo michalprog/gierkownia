b9f5397fe4d08ab73fc4690fa5ad06324b2279b7
michalprog <michalch001@gmail.com>
2023-07-28 19:22:29 +0200

    poprawka bot_33

--- lib/33_klasa.dart
+++ lib/33_klasa.dart
@@ -1,46 +1,34 @@
 import 'package:flutter/material.dart';
-class Klasa33{
-int typ_gry=0;
-int liczba_finalna=33;
-int liczba_aktualna=0;
-bool is_started=false;
-  Klasa33()
-  {
 
+class Klasa33 {
+  int typ_gry = 0;
+  int liczba_finalna = 33;
+  int liczba_aktualna = 0;
+  bool is_started = false;
+  Klasa33() {}
+  void Start(int typGry, int numer) {
+    if (numer > 33) {
+      this.liczba_finalna = numer;
+    } else {
+      this.liczba_finalna = 33;
+    }
+    this.typ_gry = typGry;
+    is_started = true;
   }
-void Start(int typGry,int numer)
-{
 
-  if (numer>33)
-  {
-  this.liczba_finalna=numer;
-  }else{
-  this.liczba_finalna=33;
+  void koniec() {
+    typ_gry = 0;
+    liczba_aktualna = 0;
+    is_started = false;
   }
-  this.typ_gry=typGry;
-  is_started=true;
-}
-void koniec(){
-    typ_gry=0;
-    liczba_aktualna=0;
-    is_started=false;
-}
-void ruch_gracza(int num)
-{
-  if(typ_gry==0){
 
-  }else if(typ_gry==1)
-    {
-
-    }else if(typ_gry==2){
-
+  void ruch_gracza(int num) {
+    if (typ_gry == 0) {
+    } else if (typ_gry == 1) {
+    } else if (typ_gry == 2) {}
   }
-}
-int wyswietl_liczbe(){
+
+  int wyswietl_liczbe() {
     return liczba_aktualna;
-}
-
-
-
-
+  }
 }
---
+++ lib/bot_33.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+class Bot_33{
+int typ=0;
+int liczba_koncowa=0;
+int liczba_aktualna=0;
+int odpowiedz=0;
+
+Bot_33(int koncowa,int typ){
+
+}
+void wprowadz_dane(){
+
+}
+
+}
