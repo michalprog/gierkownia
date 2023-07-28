921b2322711ab19a49fca107860a9076abe9c561
michalprog <michalch001@gmail.com>
2023-07-28 12:34:36 +0200

    dodanie bota

---
+++ lib/33_gra.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+class Screen_gra extends StatefulWidget {
+  const Screen_gra({Key? key}) : super(key: key);
+
+  @override
+  State<Screen_gra> createState() => _Screen_graState();
+}
+
+class _Screen_graState extends State<Screen_gra> {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child:Column(
+          children:[
+            const SizedBox(
+              height: 80,
+            ),
+
+
+
+          ]
+      )
+    );
+  }
+}
--- lib/33_klasa.dart
+++ lib/33_klasa.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/material.dart';
 class Klasa33{
 int typ_gry=0;
 int liczba_finalna=33;
@@ -9,19 +10,31 @@ bool is_started=false;
   }
 void Start(int typGry,int numer)
 {
-  this.typ_gry=typGry;
+
   if (numer>33)
   {
   this.liczba_finalna=numer;
   }else{
   this.liczba_finalna=33;
   }
+  this.typ_gry=typGry;
   is_started=true;
 }
 void koniec(){
     typ_gry=0;
     liczba_aktualna=0;
     is_started=false;
+}
+void ruch_gracza(int num)
+{
+  if(typ_gry==0){
+
+  }else if(typ_gry==1)
+    {
+
+    }else if(typ_gry==2){
+
+  }
 }
 int wyswietl_liczbe(){
     return liczba_aktualna;
