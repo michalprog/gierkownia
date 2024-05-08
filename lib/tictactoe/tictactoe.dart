d59e0a34384a64f60e0f543eaabf6f4d3daaea26
michalprog <michalch001@gmail.com>
2024-05-09 00:14:04 +0200

    popup min

--- lib/tictactoe/tictactoe.dart
+++ lib/tictactoe/tictactoe.dart
@@ -12,92 +12,115 @@ class KoloKrzyzyk extends StatefulWidget {
 class _KoloKrzyzykState extends State<KoloKrzyzyk> {
   String nazwa_gry = "kółko i krzyżyk";
   TicTacToeClass TicTactoegame = new TicTacToeClass();
-  int Index=0;
+  int Index = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Center(child: Text("kółko i krzyżyk")),
+        actions: <Widget>[
+          PopupMenuButton<String>(
+              icon: const IconButton(
+                icon: Icon(Icons.menu),
+                onPressed: null,
+              ),
+              itemBuilder: (BuildContext context) => <PopupMenuEntry<String>>[
+                    PopupMenuItem<String>(
+                        value: "1v1", child: Center(child: Text("nowy 1 vs 1"))),
+                PopupMenuItem<String>(
+                    value: "1v1", child: Center(child: Text("nowa gra łatwy"))),
+                PopupMenuItem<String>(
+                    value: "1v1", child: Center(child: Text("nowa gra trudny"))),
+                  ])
+        ],
       ),
       body: ListView(
         children: [
           Container(
-
               height: 600,
               width: 600,
               child: GridView.count(
                 crossAxisCount: 3,
                 children: [
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(0),
+                    tapaction: () => tapaction(0),
                     tekst: TicTactoegame.get_position(0),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(1),
+                    tapaction: () => tapaction(1),
                     tekst: TicTactoegame.get_position(1),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(2),
+                    tapaction: () => tapaction(2),
                     tekst: TicTactoegame.get_position(2),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(3),
+                    tapaction: () => tapaction(3),
                     tekst: TicTactoegame.get_position(3),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(4),
+                    tapaction: () => tapaction(4),
                     tekst: TicTactoegame.get_position(4),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(5),
+                    tapaction: () => tapaction(5),
                     tekst: TicTactoegame.get_position(5),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(6),
+                    tapaction: () => tapaction(6),
                     tekst: TicTactoegame.get_position(6),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(7),
+                    tapaction: () => tapaction(7),
                     tekst: TicTactoegame.get_position(7),
                   ),
                   WindowTictactoe(
-                    tapaction: ()=>tapaction(8),
+                    tapaction: () => tapaction(8),
                     tekst: TicTactoegame.get_position(8),
                   ),
-
                 ],
               )),
-Spacer(),
-         Container(
-           child: Row(
-             children: [
-               ElevatedButton(onPressed: (){startgame(1);}, child: Text("1 vs 1")),
-               Spacer(),
-               ElevatedButton(onPressed: (){startgame(2);}, child: Text("bot łatwy")),
-               Spacer(),
-               ElevatedButton(onPressed: (){startgame(3);}, child: Text("bot trudny")),
-             ],
-           ),
-
-         )
+          Spacer(),
+          Container(
+            child: Row(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      startgame(1);
+                    },
+                    child: Text("1 vs 1")),
+                Spacer(),
+                ElevatedButton(
+                    onPressed: () {
+                      startgame(2);
+                    },
+                    child: Text("bot łatwy")),
+                Spacer(),
+                ElevatedButton(
+                    onPressed: () {
+                      startgame(3);
+                    },
+                    child: Text("bot trudny")),
+              ],
+            ),
+          )
         ],
       ),
-
     );
   }
-  void tapaction(int position){
-    if(TicTactoegame.game_started) {
+
+  void tapaction(int position) {
+    if (TicTactoegame.game_started) {
       setState(() {
         TicTactoegame.move(position);
         print("dzialam");
       });
     }
   }
-  void startgame(int rodzaj){
 
-      setState(() {
-        TicTactoegame.newgame(rodzaj);
-      });
-
+  void startgame(int rodzaj) {
+    setState(() {
+      TicTactoegame.newgame(rodzaj);
+    });
   }
 }
